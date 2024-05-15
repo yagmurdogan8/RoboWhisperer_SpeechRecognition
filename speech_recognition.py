0e51b90bc1281b2ecd139c9089c8fa5cbeb017b4
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2024-05-15 17:24:57 +0300

    mic_test has been added

--- speech_recognition.py
+++ speech_recognition.py
@@ -4,7 +4,24 @@ import pyaudio
 names = ["Giovanni", "Barbaros", "Rajeck", "Joren", "Yağmur"]
 recognizer = sr.Recognizer()
 
-# Function to listen for a voice command
+def test_microphone():
+    with sr.Microphone() as source:
+        print("Adjusting for ambient noise, please wait...")
+        recognizer.adjust_for_ambient_noise(source, duration=5)
+        print("Say something!")
+        audio = recognizer.listen(source)
+        
+        try:
+            # Recognize the audio using Google Web Speech API
+            print("Google Speech Recognition thinks you said: " + recognizer.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand the audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+
+test_microphone()
+
+
 def listen_for_command():
     with sr.Microphone() as source:
         print("Listening for a command...")
